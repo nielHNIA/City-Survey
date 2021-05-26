cbda495526272875d1adb503c2f03e6f97a1a8fe
nielHNIA <niel.sharma805@gmail.com>
2021-05-26 15:20:25 +0530

    coon

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn new_project.wsgi
+web: gunicorn new_project.wsgi --log-file -
--- new_project/settings.py
+++ new_project/settings.py
@@ -80,16 +80,16 @@ REST_FRAMEWORK = {
 }"""
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 
-
+"""
 DATABASES = {
 
     'default': {
@@ -109,6 +109,7 @@ DATABASES = {
     }
 
 }
+"""
 
 
 
